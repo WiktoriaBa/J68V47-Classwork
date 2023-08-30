88200246855eedda27de07b1e8fb00664048bdee
30160329 <30160329@nescol.ac.uk>
2023-08-30 15:39:08 +0100

    Haiku

---
+++ src/L1E1.java
@@ -0,0 +1,7 @@
+public class L1E1 {
+    public static void main(String[] args) {
+        System.out.println("Summer's end is near");
+        System.out.println("The cicadas loudly sing");
+        System.out.println("Leaves begin to fall");
+    }
+}
