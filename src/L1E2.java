c51303da40a75d93fafe98ce978dc038455e30cd
30160329 <30160329@nescol.ac.uk>
2023-08-30 15:46:32 +0100

    Haiku

---
+++ src/L1E2.java
@@ -0,0 +1,9 @@
+public class L1E2 {
+    public static void main(String[] args) {
+        System.out.println("bailie sit = Abilities");
+        System.out.println("adobes bar = Baseboard");
+        System.out.println("be took tx = textbook");
+        System.out.println("chit queen = technique");
+        System.out.println("lazy pared = Paralyzed");
+    }
+}
