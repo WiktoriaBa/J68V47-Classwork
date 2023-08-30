cec9861988c8b2145b50bfa0823de2335d35a0af
30160329 <30160329@nescol.ac.uk>
2023-08-30 16:05:12 +0100

    Anagrams again

---
+++ src/L1E2.java
@@ -0,0 +1,10 @@
+public class L1E2 {
+    public static void main(String[] args) {
+        System.out.println("Adora ebbs = Baseboard");
+        System.out.println("chit queen = technique");
+        System.out.println("be took tx = Textbook");
+        System.out.println("gird Liz zn = Drizzling");
+        System.out.println("exert pies = Expertise");
+
+    }
+}
