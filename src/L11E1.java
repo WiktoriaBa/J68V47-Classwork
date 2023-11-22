895e6396e7c223838aadf7d08a66a38a39406aeb
wiktoria <30160329@nescol.ac.uk>
2023-11-22 14:18:59 +0000

    L11

---
+++ src/L11E1.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+import java.io.*;
+public class L11E1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Input a base number: ");
+        int base = input.nextInt();
+        int count;
+        try (PrintWriter out = new PrintWriter( new FileWriter("output.txt") ) ) {
+            for (count = 1; count <= 12; count += 1) {
+            out.println(base + " x " + count + " = " + (count * base)); }
+            out.close();
+        } catch (IOException e) {
+            System.out.println("Error occurred writing to file: " + e.toString());
+        }
+
+
+
+
+
+
+
+
+    }
+}
