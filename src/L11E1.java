06ab780ec693dd206866ea68fc230129ff6a92ee
30160329 <30160329@nescol.ac.uk>
2023-11-27 09:23:07 +0000

    L11

--- src/L11E1.java
+++ src/L11E1.java
@@ -5,9 +5,8 @@ public class L11E1 {
         Scanner input = new Scanner(System.in);
         System.out.print("Input a base number: ");
         int base = input.nextInt();
-        int count;
         try (PrintWriter out = new PrintWriter( new FileWriter("output.txt") ) ) {
-            for (count = 1; count <= 12; count += 1) {
+            for (int count = 1; count <= 12; count += 1) {
             out.println(base + " x " + count + " = " + (count * base)); }
         } catch (IOException e) {
             System.out.println("Error occurred writing to file: " + e);
