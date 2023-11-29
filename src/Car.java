9bb94077abb6042d25a12cb0dc0e3d9af0cffe45
30160329 <30160329@nescol.ac.uk>
2023-11-29 15:33:21 +0000

    L12

---
+++ src/Car.java
@@ -0,0 +1,13 @@
+public class Car {
+    public String make = "";
+    public String model = "";
+    public int year = 0;
+    public String vin = "";
+    public double price = 0.0;
+    public String colour = "";
+    public int mileage = 0;
+    public String condition = "";
+
+
+
+}
---
+++ src/L12E3.java
@@ -0,0 +1,18 @@
+public class L12E3 {
+    public static void main(String[] args) {
+        Car car1 = new Car();
+
+        car1.make ="Ferrari";
+        car1.model = "F430";
+        car1.year = 2009;
+        car1.vin = "3GYVKNEFXAG625569";
+        car1.price = 55125;
+        car1.colour= "white";
+        car1.mileage= 45336;
+        car1.condition= "good";
+
+
+
+
+    }
+}
