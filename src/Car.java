a0f3f24905aad7c9ec270b83499b0e3b7de9ab7a
wiktoria <30160329@nescol.ac.uk>
2023-12-03 10:47:17 +0000

    L12

--- src/Car.java
+++ src/Car.java
@@ -3,7 +3,7 @@ public class Car {
     public String model = "";
     public int year = 0;
     public String vin = "";
-    public double price = 0.0;
+    public int price = 0;
     public String colour = "";
     public int mileage = 0;
     public String condition = "";
--- src/L12E3.java
+++ src/L12E3.java
@@ -1,18 +1,22 @@
 public class L12E3 {
+    public static void displaycar(Car car1) {
+        System.out.format("(%d)%nVin: %s%nColour: %s%nMileage: %d%nCondition: %s%nPrice: %d%n",
+                car1.year, car1.vin, car1.colour, car1.mileage, car1.condition, car1.price);
+        System.out.println("--------------------------------------");
+    }
+
     public static void main(String[] args) {
         Car car1 = new Car();
+        displaycar(car1);
 
-        car1.make ="Ferrari";
-        car1.model = "F430";
+        car1.make = "Ferrari";
+        car1.model = "F439";
         car1.year = 2009;
         car1.vin = "3GYVKNEFXAG625569";
         car1.price = 55125;
-        car1.colour= "white";
-        car1.mileage= 45336;
-        car1.condition= "good";
-
-
-
-
+        car1.colour = "White";
+        car1.mileage = 45336;
+        car1.condition = "Good";
+        displaycar(car1);
     }
 }
